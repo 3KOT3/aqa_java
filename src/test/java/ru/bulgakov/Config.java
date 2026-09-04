c56af5fbda11c3562a18824cbb91990c00d931f3
Владислав Юстус <V.Yustus@m-developer.ru>
2026-09-04 17:26:09 +0300

    review 2

--- src/test/java/ru/bulgakov/Config.java
+++ src/test/java/ru/bulgakov/Config.java
@@ -5,7 +5,4 @@ public class Config {
     public static final String YANDEX_URL = "https://ya.ru/";
     public static final String WIKIPEDIA_URL = "https://ru.wikipedia.org/";
     public static final String COURSE_HOST = "ivanbulgakovqa.ru";
-
-    private Config() {
-    }
 }
