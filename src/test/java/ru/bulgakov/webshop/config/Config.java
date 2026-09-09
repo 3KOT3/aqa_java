3da790a35873800a4bb554c31a2b54cd948a0b56
3KOT3 <sergeylllilllkin2@gmail.com>
2026-09-09 17:03:23 +0300

    all fix

--- src/test/java/ru/bulgakov/webshop/config/Config.java
+++
@@ -1,6 +0,0 @@
-package ru.bulgakov.webshop.config;
-
-public class Config {
-    public static final String WEB_SHOP_URL = "https://demowebshop.tricentis.com/";
-    public static final String WEB_SHOP_REGISTRATION_URL = WEB_SHOP_URL + "register";
-}
